04496a6bd4c9d259f3169a37be2c43c4938129ad
jminjares4 <minjares.jesus.ths@gmail.com>
2022-05-04 09:45:38 -0600

    Update motor_plot.py with click commands

--- tools/motor_plot.py
+++ tools/motor_plot.py
@@ -1,21 +1,37 @@
 # Import matplotlib and pandas
+
 import matplotlib.pyplot as plt
+from numpy import require
 import pandas as pd
+import click
 
-# store dataframe
-data = pd.read_csv("motor.csv")
+# example
+# python3 motor_plot.py --file-name="motor.csv" --images="pwm.png" --images="encoder.png"
 
-# print dataframe 
-print(data.head())
+@click.command()
+@click.option('--file-name', '-f', required=True, help='File Name')
+@click.option('--images', '-i',multiple=True, default=['json'],help='Images Names')
+def main(file_name, images):
+    # store dataframe
+    data = pd.read_csv(file_name)
 
-# Plot and store pwm_input
-data.plot(x='time', y='pwm_input', kind='line')
-plt.xlabel('Sample', fontsize=10, fontweight='bold')
-plt.ylabel('PWM Input', fontsize=10, fontweight='bold')
-plt.savefig('pwm_input.png')
+    # print dataframe 
+    print(data.head())
 
-# Plot and store encoder
-data.plot(x='time', y='encoder', kind='line')
-plt.xlabel('Sample', fontsize=10, fontweight='bold')
-plt.ylabel('Encoder position', fontsize=10, fontweight='bold')
-plt.savefig('encoder_position.png')
+    if len(images) == 2:
+        
+
+        # Plot and store pwm_input
+        data.plot(x='time', y='pwm_input', kind='line')
+        plt.xlabel('Sample', fontsize=10, fontweight='bold')
+        plt.ylabel('PWM Input', fontsize=10, fontweight='bold')
+        plt.savefig(images[0])
+
+        # Plot and store encoder
+        data.plot(x='time', y='encoder', kind='line')
+        plt.xlabel('Sample', fontsize=10, fontweight='bold')
+        plt.ylabel('Encoder position', fontsize=10, fontweight='bold')
+        plt.savefig(images[1])
+
+if __name__=="__main__":
+    main()
