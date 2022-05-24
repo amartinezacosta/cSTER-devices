e3ec045688ef5ec56f7a7a778c33ffd365520bbd
jminjares4 <minjares.jesus.ths@gmail.com>
2022-05-24 16:42:49 -0600

    Added comments to source code

--- ppm/examples/ppm_radio_link.c
+++ ppm/examples/ppm_radio_link.c
@@ -15,23 +15,28 @@
 
 int main(void)
 {
+    /*Find PPM device*/
     dev_t *capture = HAL_find_name("CAP_0");
     HAL_ASSERT_MODULE_NAME(capture, "main");
 
+    /*Create an instance of PPM object*/
     ppm_t ppm;
     ppm_ctor(&ppm , capture, 8);
 
     uint32_t channels[8];
     while(1)
     {
+        /*Read PPM channels*/
         ppm_get_channels(&ppm, channels);
 
+        /*Log information*/
         log_info("%i,%i,%i,%i,%i,%i,%i,%i",
                  channels[0], channels[1],
                  channels[2], channels[3],
                  channels[4], channels[5],
                  channels[6], channels[7]);
-
+        
+        /*10 millisecond delay*/
         SysTick_delay(10);
     }
 }
